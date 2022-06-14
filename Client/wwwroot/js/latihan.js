bf740d8be6c9fb455d6cbacace6dd85104027c14
rizki_fauzi24 <mrizkifauzi24@gmail.com>
2022-06-14 11:57:46 +0700

    update UI

---
+++ Client/wwwroot/js/latihan.js
@@ -0,0 +1,122 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+console.log("test latihan");
+
+let array = [1, 2, 3, "test"];
+console.log(array);
+
+
+array.push("halo");
+console.log(array);
+
+array.pop();
+console.log(array);
+
+array.unshift("depan");
+console.log(array);
+
+array.shift();
+console.log(array);
+
+console.log("array MULTI")
+let arrayMulti = ['a', 'b', 'c', [1, 2, 3], true];
+console.log(arrayMulti[3][1]);
+
+//deklarasi object
+let mhs = {
+    nama: "rizki",
+    nim: "170914060",
+    jurusan: "TI",
+    umur: 23,
+    hobi: ["mancing", "ngegame", "olahraga"],
+    isActive: true
+}
+
+console.log(mhs);
+
+console.log(mhs.hobi[2]);
+
+const hobis = [];
+for (var i = 0; i < mhs.hobi.length; i++) {
+    hobis.push(mhs.hobi[i]);
+}
+
+console.log(hobis);
+
+
+const user = {};
+
+user.username = "rizki";
+user.nrp = "170914060";
+
+console.log(user.nrp);
+
+user.username = "fajrin";
+console.log(user);
+
+key = "password";
+console.log(user[key]);
+
+const csv = "1|2|3";
+
+const [one, two, three] = csv.split("|");
+console.log(three);
+
+
+
+//jika species cat masukan ke dalam variable onlycat
+//jika species fish masukan ke dalam ganti classnya jadi non mamalia
+
+//let onlyCat = [];
+
+//console.log(onlyCat)
+
+//animals.forEach(function (animals) {
+//    if (animals.species == "cat") {
+//        onlyCat.push(animals)
+//    } else {
+//        animals.class.name = "non-invertebrata";
+//    }
+//})
+
+//onlyCat.forEach(function (cat) {
+//    console.log(cat);
+//})
+const animals = [
+    { name: "Garfield", species: "cat", class: { name: "mamalia" } },
+    { name: "Nemo", species: "fish", class: { name: "invertebrata" } },
+    { name: "Tom", species: "cat", class: { name: "mamalia" } },
+    { name: "Bruno", species: "fish", class: { name: "invertebrata" } },
+    { name: "Carlo", species: "cat", class: { name: "mamalia" } },
+]
+console.log(animals);
+
+const OnlyCat = [];
+
+for (var i = 0; i < animals.length; i++) {
+    if (animals[i].species == "fish") {
+        animals[i].class.name = "Non-Mamalia";
+    }
+    else if (animals[i].species == "cat") {
+        OnlyCat.push(animals[i]);
+    }
+}
+
+console.log("Animals");
+console.log(animals);
+
+console.log("Cats");
+console.log(OnlyCat);
+
+
+
+
+
+
+
+
+
+
+
--- Client/wwwroot/js/site.js
+++ Client/wwwroot/js/site.js
@@ -2,3 +2,160 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+//const currentLocation = location.href;
+//const menuItem = document.querySelectorAll('a');
+//const menuLength = menuItem.length;
+//for (let i = 0; i < menuLength; i++) {
+//    if (menuItem[i].href === currentLocation) {
+//        menuItem[i].className = "active";
+//    } else {
+//        menuItem[i].className = "";
+//    }
+//}
+
+//const myElement = document.getElementById("card1");
+//myElement.className = "card text-white bg-danger mb-3";
+
+//document.getElementById("card1").addEventListener("click", ubahWarna1);
+
+function ubahWarna1() {
+    const getClass = document.getElementById("card1");
+    if (getClass.className == "card text-white bg-info mb-3") {
+        document.getElementById("card1").className = "card text-white bg-warning mb-3";
+    } else {
+        document.getElementById("card1").className = "card text-white bg-info mb-3";
+    }
+}
+
+//document.getElementById("btn2").addEventListener("paragraf", ubahParagraf);
+
+function ubahParagraf() {
+    const getPar = document.getElementById("paragraf");
+    const getBtn = document.getElementById("btn2");
+    if (getPar.className == "card-text text-justify") {
+        document.getElementById("paragraf").className = "card-text text-left";
+        getBtn.innerHTML = "Change to Rigth";
+
+    } else if (getPar.className == "card-text text-left") {
+        document.getElementById("paragraf").className = "card-text text-right";
+        getBtn.innerHTML = "Change to Center";
+    } else if (getPar.className == "card-text text-right") {
+        document.getElementById("paragraf").className = "card-text text-center";
+        getBtn.innerHTML = "Change to Justify";
+    } else {
+        document.getElementById("paragraf").className = "card-text text-justify";
+        getBtn.innerHTML = "Change to Left";
+    }
+}
+
+function ubahWarna() {
+    const getClass = document.getElementById("card2");
+    const getButton = document.getElementById("btn1");
+    if (getButton.value == "red") {
+        getButton.value = "yellow"
+        getClass.className = "card text-white bg-warning mb-3";
+        getButton.className = "btn btn-warning";
+        getButton.innerHTML = "Change to Green";
+    } else if (getButton.value == "yellow") {
+        getButton.value = "green"
+        getClass.className = "card text-white bg-success mb-3";
+        getButton.className = "btn btn-success";
+        getButton.innerHTML = "Change to Blue";
+    } else if (getButton.value == "green") {
+        getButton.value = "blue"
+        getClass.className = "card text-white bg-primary mb-3";
+        getButton.className = "btn btn-primary";
+        getButton.innerHTML = "Change to Red";
+    } else {
+        getButton.value = "red"
+        getClass.className = "card text-white bg-danger mb-3";
+        getButton.className = "btn btn-danger";
+        getButton.innerHTML = "Change to Yellow";
+    }
+}
+
+//JQUERY
+
+
+$.ajax({
+    url: "https://pokeapi.co/api/v2/pokemon/",
+}).done((result) => {
+    console.log(result.results);
+    let text = "";
+    $.each(result.results, function (key, val) {
+        console.log(val.name);
+        text += `<tr>
+                    <td>${key+1}</td>
+                    <td>${val.name}</td>
+                    <td>
+                        <button type="button" onclick="detailPoke('https://pokeapi.co/api/v2/pokemon/${val.name}')" class="btn btn-primary" data-toggle="modal" data-target="#modalDetail">
+                                Detail
+                        </button>
+                    </td>
+                </tr>`
+    });
+    //console.log(text);
+    $("#tablePoke").html(text);
+}).fail((error) => {
+    console.log(error);
+})
+
+function detailPoke(url) {
+    $.ajax({
+        url
+    }).done(e => {
+        console.log(e);
+        $("#imgPoke").attr("src", e.sprites.other.dream_world.front_default);
+        $("#imgPoke2").attr("src", e.sprites.other.home.front_default);
+        $("#imgPoke3").attr("src", e.sprites.other.home.front_shiny);
+        $(".namePoke").text(e.name);
+        $("#base_experience").width(e.base_experience);
+        $("#height").text(e.height);
+        $("#weight").text(e.weight);
+        $("#order").text(e.order);
+        $("#abil").text(e.abilities[0].ability.name);
+        $("#hp").width(e.stats[0].base_stat+"%");
+        $("#attack").width(e.stats[1].base_stat+"%");
+        $("#defense").width(e.stats[2].base_stat+"%");
+        $("#special-attack").width(e.stats[3].base_stat+"%");
+        $("#special-defense").width(e.stats[4].base_stat+"%");
+        $("#speed").width(e.stats[5].base_stat + "%");
+
+        texts = "",
+            $.each(e.types, function (val) {
+                texts += `<span class="badge badge-pill badge-success">${e.types[val].type.name}</span>`
+            });
+        console.log(texts);
+        $("#types").html(texts);
+
+
+        
+    })
+    //console.log(url);
+    //$("#imgPoke").html();
+}
+
+//$.ajax({
+//    url: "https://pokeapi.co/api/v2/pokemon/",
+//}).done((res) => {
+//    console.log(res.results);
+//    let text = "";
+//    $.each(res.results, function (key, val) {
+//        console.log(val.abilities);
+        
+//        //text += `<tr>
+//        //            <td>${key + 1}</td>
+//        //            <td>${val.name}</td>
+//        //            <td>
+//        //                <button type="button" onclick="detailPoke('https://pokeapi.co/api/v2/pokemon/${val.name}')" class="btn btn-primary" data-toggle="modal" data-target="#modalDetail">
+//        //                        Detail
+//        //                </button>
+//        //            </td>
+//        //        </tr>`
+//    });
+//    //console.log(text);
+//    //$("#tablePoke").html(text);
+//}).fail((error) => {
+//    console.log(error);
+//})
+
